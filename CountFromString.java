37b4f024baad390e31bcd191599985b8b3d2bef8
Piyush Yadav <piyushkrishna11@gmail.com>
2024-12-24 15:28:51 +0000

    basic start

---
+++ CountFromString.java
@@ -0,0 +1,14 @@
+public class CountFromString {
+    public static void main(String[] args) {
+        int n = 834622390;
+        int count = 0;
+        while (n>0) {
+            int rem = n% 10;
+            if(rem ==2){
+                count++;
+            }
+            n = n/10;
+        }
+        System.out.println(count);
+    }
+}
